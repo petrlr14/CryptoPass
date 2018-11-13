14f3ba2e9cc2415b2ddcc266d7b622655aa966ec
CarlosDubon <00058016@uca.edu.sv>
2018-11-13 17:01:50 -0600

    fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import {SignUp} from './components/SignUp.js';
 import './assets/css/bootstrap.css';
 import './assets/css/style.css';
-
+ 
 ReactDOM.render(
     <SignUp/>,
     document.getElementById("root")
