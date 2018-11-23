f767e68c25042592710132653a8ccbccb9e54bdf
CarlosDubon <00058016@uca.edu.sv>
2018-11-23 11:00:15 -0600

    fix

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import {App} from './app';
 import './assets/css/bootstrap.css';
 import './assets/css/style.css';
 import 'jquery/dist/jquery.min';
-//import 'popper.js';
-import 'bootstrap'
+import 'popper.js';
+import 'bootstrap';
 
 
  
