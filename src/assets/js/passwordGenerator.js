91b2cbf95cc46cce88f8223797a98bcc5090b25d
CarlosDubon <00058016@uca.edu.sv>
2018-11-23 14:25:40 -0600

    ultimos cambios

--- src/assets/js/passwordGenerator.js
+++ src/assets/js/passwordGenerator.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function () {
     var lengthSlider = document.getElementById("size");
     var sliderValue = document.getElementById("sizeValue");
-    sliderValue.innerHTML = lengthSlider.value;
+    //sliderValue.innerHTML = lengthSlider.value;
 
     var lowerCaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     var upperCaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
